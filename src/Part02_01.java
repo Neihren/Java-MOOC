70a42d93e3aeb2f003d5ab784a9e40448102604f
Neihren <185206470+Neihren@users.noreply.github.com>
2024-12-09 16:57:25 +0100

    part 2

---
+++ src/Part02_01.java
@@ -0,0 +1,9 @@
+public class Part02_01 {
+    public static void main(String[] args) {
+        printText();
+    }
+
+    public static void printText() {
+        System.out.println("In a hole in the groud there lived a method");
+    }
+}
---
+++ src/Part02_02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Part02_02 {
+    public static void main(String[] args) {
+        Scanner scan;
+        int numberOfRepeat;
+
+        scan = new Scanner(System.in);
+        
+        System.out.println("How Many times ?");
+        numberOfRepeat = scan.nextInt();
+        printText(numberOfRepeat);
+        scan.close();
+    }
+
+    public static void printText(int repeat) {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("In a hole in the groud there lived a method");
+        }
+    }
+}
